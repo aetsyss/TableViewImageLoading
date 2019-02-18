59f4f64a4e220aa4629030ffa5f7a921284fd3ca
Aleksei Tsyss <Aleksei_Tsyss@epam.com>
2019-02-18 15:17:37 +0300

    initial commit

---
+++ tableviewimageloading/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tableviewimageloading
+//
+//  Created by Aleksei Tsyss on 18/02/2019.
+//  Copyright © 2019 Aleksei Tsyss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tableviewimageloading/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  tableviewimageloading
+//
+//  Created by Aleksei Tsyss on 18/02/2019.
+//  Copyright © 2019 Aleksei Tsyss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
